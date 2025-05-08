aae3db8ff30d34f56e782c1b22cd9a11920f05b7
schen011 <sophiachen805@gmail.com>
2025-05-08 02:44:04 -0400

    recent package + llm
    again. god save me.

--- Assets/code/servePerson.cs
+++ Assets/code/servePerson.cs
@@ -9,6 +9,10 @@ public class servePerson : MonoBehaviour
 
     public TextMeshProUGUI scoreText;
 
+    public gameFlow gameflow;
+
+    [SerializeField] private timerScipt timerScript;
+
     private void OnTriggerEnter(Collider other)
     {
       food_script data = other.GetComponent<food_script>();
@@ -34,33 +38,22 @@ public class servePerson : MonoBehaviour
             Debug.Log("correct" +" "+ gameFlow.orderTimer);
             AddScore(10);
             CustomerManager.Instance.SellToCustomer();
+            gameflow.NewOrder();
+            timerScript.ResetTimer(gameFlow.orderTimer);   
+
         }
         else
         {
             Debug.Log("incorrect");
             
         }
-
-        // if(gameFlow.orderValue == gameFlow.personValue)
-        // {
-        //     Debug.Log("correct" +" "+ gameFlow.orderTimer);
-        //     AddScore(10);
-        // }
-        // else
-        // {
-        //     Debug.Log("incorrect");
-            
-        // }
     }
-
-
-
 
 
     void AddScore(int amount)
     {
         score += amount;
-        UImanager.instance.UpdateUI(score);
+        UImanager.instance.UpdateUI();
         scoreText.text = "Score: " + score.ToString(); // update TMP text
     }
 
